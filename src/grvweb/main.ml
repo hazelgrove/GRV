11f415f043b5f5b3ba26fe2f81333ddc81147e14
Eric Griffis <dedbox@gmail.com>
2020-04-28 22:06:43 -0700

    rearrange Start_app.start arguments

--- src/grvweb/main.ml
+++ src/grvweb/main.ml
@@ -1,4 +1,4 @@
 let () =
-  Incr_dom.Start_app.start ~bind_to_element_with_id:"container"
-    ~initial_model:(Model.init ())
+  Incr_dom.Start_app.start
     (module App)
+    ~bind_to_element_with_id:"container" ~initial_model:(Model.init ())

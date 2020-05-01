cd6c95842c39d1232de5fa4faff060f71e8f8d68
Eric Griffis <dedbox@gmail.com>
2020-05-01 16:30:05 -0700

    document Vertex module

--- src/grvcore/vertex.ml
+++ src/grvcore/vertex.ml
@@ -1,6 +1,9 @@
-(** A vertex in the program graph *)
+(** Graph vertices
+
+    A [Vertex] encodes an {!Ast.HExp} constructor sort and type. *)
 
 module Vertex = struct
+  (** Indicates the sort and type of an [HExp] constructor. *)
   type ctor =
     | Root_root
     | Id_id of string
@@ -13,14 +16,20 @@ module Vertex = struct
 
   type t = ctor Uuid.t
 
-  let root : t = { uuid = 0; value = Root_root }
-
   let mk (ctor : ctor) : t = Uuid.wrap ctor
 
+  (** [Vertex]es are compared by comparing their {!field:Uuid.t.id}s *)
   let compare : t -> t -> int = Uuid.compare
+
+  (** The root [Vertex] of all graphs. *)
+  let root : t = { id = 0; value = Root_root }
+
+  (** {1 Pretty Printing} *)
 
   let pp : Format.formatter -> t -> unit = Uuid.pp pp_ctor
 end
 
 include Vertex
+
 module Map = Map.Make (Vertex)
+(** An abstract map with [Vertex]es for keys *)

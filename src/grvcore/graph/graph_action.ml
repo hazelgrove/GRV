612cfd0ad51f2345a193edf16ae64b180b966545
Eric Griffis <dedbox@gmail.com>
2020-05-25 11:46:18 -0700

    clean up Graph_action.apply code

--- src/grvcore/graph/graph_action.ml
+++ src/grvcore/graph/graph_action.ml
@@ -6,29 +6,22 @@ let pp (fmt : Format.formatter) (edge_action : t) : unit =
     edge.source
     (Uuid.Id.show edge.target.id)
 
-let apply (edge_action : t) (graph : Graph.t) : Graph.t =
-  let edge = edge_action.edge in
-  let edge_state = edge_action.state in
-  let old_state = Edge.Map.find_opt edge graph.states in
-  let action : Edge_state.t option =
-    match old_state with
-    | Some Destroyed -> None
-    | Some Created -> (
-        match edge_state with Destroyed -> Some Destroyed | Created -> None )
-    | None -> Some edge_state
-  in
-  match action with
-  | None -> graph
-  | Some Created ->
-      (* TODO: assert not already exists? *)
-
-      (* TODO: short circuit if deleting a non-existant *)
-      let states = Edge.Map.add edge Edge_state.Created graph.states in
-      { states; cache = Cache.create edge graph.cache }
-  | Some Destroyed -> (
+let apply (action : t) (graph : Graph.t) : Graph.t =
+  let old_state = Edge.Map.find_opt action.edge graph.states in
+  let new_state = action.state in
+  match (old_state, new_state) with
+  | Some Destroyed, _ -> graph
+  | Some Created, Created -> graph
+  | (Some Created | None), Destroyed -> (
       let states : Edge_state.t Edge.Map.t =
-        Edge.Map.add edge Edge_state.Destroyed graph.states
+        Edge.Map.add action.edge Edge_state.Destroyed graph.states
       in
       match old_state with
       | None -> Graph.mk states graph.cache
-      | _ -> { states; cache = Cache.destroy edge graph.cache } )
+      | _ -> { states; cache = Cache.destroy action.edge graph.cache } )
+  | None, Created ->
+      (* TODO: assert not already exists? *)
+
+      (* TODO: short circuit if deleting a non-existant *)
+      let states = Edge.Map.add action.edge Edge_state.Created graph.states in
+      { states; cache = Cache.create action.edge graph.cache }

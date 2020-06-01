ab1114805a287a309bab66a39830568f36e72227
Michael D. Adams <adamsmd@users.noreply.github.com>
2020-06-01 15:30:49 -0400

    Simplify a let%map

--- src/grvweb/App.ml
+++ src/grvweb/App.ml
@@ -13,7 +13,6 @@ let on_startup ~schedule_action:(_ : Action.t -> unit) (_ : Model.t) :
 let create (model : Model.t Incr.t) ~old_model:(_ : Model.t Incr.t)
     ~(inject : Action.t -> Vdom.Event.t) :
     (Action.t, Model.t, State.t) Component.t Incr.t =
-  let open Incr.Let_syntax in
-  let%map model = model in
+  let%map.Incr model = model in
   let view = View.view ~inject model in
   Component.create model view ~apply_action:(Action.apply model)

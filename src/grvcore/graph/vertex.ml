301603847177b2e4c58cbf7f88af46e8d02dca38
Michael D. Adams <adamsmd@users.noreply.github.com>
2020-06-01 14:05:27 -0400

    Minor work on compare

--- src/grvcore/graph/vertex.ml
+++ src/grvcore/graph/vertex.ml
@@ -2,11 +2,13 @@ type t = Lang.Constructor.t Uuid.Wrap.t [@@deriving show, sexp_of]
 
 let mk : Lang.Constructor.t -> t = Uuid.Wrap.mk
 
-let root : t = Uuid.Wrap.well_known 0 Lang.Constructor.Root_root
+let pp : Format.formatter -> t -> unit = Uuid.Wrap.pp Lang.Constructor.pp
+
+(* TODO: let equal : t -> t -> bool = Uuid.Wrap.equal *)
 
 let compare : t -> t -> int = Uuid.Wrap.compare
 
-let pp : Format.formatter -> t -> unit = Uuid.Wrap.pp Lang.Constructor.pp
+let root : t = Uuid.Wrap.well_known 0 Lang.Constructor.Root_root
 
 module OrderedType = struct
   type nonrec t = t

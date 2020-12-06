dfe2f84d3a7a716765ab733e19fa68267cc051c4
Eric Griffis <egriffis@umich.edu>
2020-12-06 18:22:40 -0500

    clean up Editor.ml code

--- src/grvcore/app/Editor.ml
+++ src/grvcore/app/Editor.ml
@@ -8,18 +8,17 @@ type t = {
 }
 
 let sexp_of_t (editor : t) : Sexplib.Sexp.t =
-  Sexplib.(
-    Sexp.List
-      [
-        Uuid.Id.sexp_of_t editor.id;
-        Graph.sexp_of_t editor.graph;
-        Cursor.sexp_of_t editor.cursor;
-        Std.sexp_of_list Graph_action.sexp_of_t
-          (Graph_action.Set.elements editor.actions);
-        Std.sexp_of_list Graph_action.sexp_of_t
-          (Graph_action.Set.elements editor.known_actions);
-        Std.sexp_of_bool editor.show_ids;
-      ])
+  Sexplib.Sexp.List
+    [
+      Uuid.Id.sexp_of_t editor.id;
+      Graph.sexp_of_t editor.graph;
+      Cursor.sexp_of_t editor.cursor;
+      Sexplib.Std.sexp_of_list Graph_action.sexp_of_t
+        (Graph_action.Set.elements editor.actions);
+      Sexplib.Std.sexp_of_list Graph_action.sexp_of_t
+        (Graph_action.Set.elements editor.known_actions);
+      Sexplib.Std.sexp_of_bool editor.show_ids;
+    ]
 
 let t_of_sexp : Sexplib.Sexp.t -> t = function
   | Sexplib.Sexp.List
@@ -35,12 +34,12 @@ let t_of_sexp : Sexplib.Sexp.t -> t = function
       let graph = Graph.t_of_sexp graph_sexp in
       let cursor = Cursor.t_of_sexp cursor_sexp in
       let actions =
-        Graph_action.Set.of_list
-          (Sexplib.Std.list_of_sexp Graph_action.t_of_sexp actions_sexp)
+        Sexplib.Std.list_of_sexp Graph_action.t_of_sexp actions_sexp
+        |> Graph_action.Set.of_list
       in
       let known_actions =
-        Graph_action.Set.of_list
-          (Sexplib.Std.list_of_sexp Graph_action.t_of_sexp known_actions_sexp)
+        Sexplib.Std.list_of_sexp Graph_action.t_of_sexp known_actions_sexp
+        |> Graph_action.Set.of_list
       in
       let show_ids = Sexplib.Std.bool_of_sexp show_ids_sexp in
       { id; graph; cursor; actions; known_actions; show_ids }
@@ -55,9 +54,3 @@ let mk () : t =
     known_actions = Graph_action.Set.empty;
     show_ids = false;
   }
-
-(* module OrderedType = struct
-  type nonrec t = t
-
-  let compare : t -> t -> int = Int.compare
-end *)

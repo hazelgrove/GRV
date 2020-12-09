5894301d040c38dcb38f26a701cf2e4fc778220b
Eric Griffis <egriffis@umich.edu>
2020-12-08 22:22:53 -0500

    rearrange Graph.ml to match mli file

--- src/grvcore/graph/Graph.ml
+++ src/grvcore/graph/Graph.ml
@@ -1,19 +1,8 @@
-(* Note: if an edge is not in the map, it has not been created yet and is equal
-   to `\bot`. *)
 type t = Edge_state.t Edge.Map.t
 
-let sexp_of_t (graph : t) : Sexplib.Sexp.t =
-  Util.Sexp.of_map (Edge.Map.bindings graph) Edge.sexp_of_t Edge_state.sexp_of_t
-
-let t_of_sexp (sexp : Sexplib.Sexp.t) : t =
-  sexp
-  |> Sexplib.Std.list_of_sexp (function
-       | List [ key_sexp; value_sexp ] ->
-           (Edge.t_of_sexp key_sexp, Edge_state.t_of_sexp value_sexp)
-       | _ -> failwith __LOC__)
-  |> List.to_seq |> Edge.Map.of_seq
-
 let empty : t = Edge.Map.empty
+
+(* Edge Queries *)
 
 let edges (graph : t) : Edge.Set.t =
   Edge.Map.bindings graph |> List.map fst |> Edge.Set.of_list
@@ -32,12 +21,6 @@ let child_edges (graph : t) (vertex : Vertex.t) (position : Lang.Position.t) :
   |> Edge.Set.filter (fun edge ->
          edge.value.source = Cursor.{ vertex; position })
 
-let parent_vertexes (graph : t) (vertex : Vertex.t) : Vertex.Set.t =
-  Vertex.Set.empty
-  |> Edge.Set.fold
-       (fun e -> Vertex.Set.add e.value.source.vertex)
-       (parent_edges graph vertex)
-
 let (vertexes, live_vertexes) : (t -> Vertex.Set.t) * (t -> Vertex.Set.t) =
   let impl (edge_source : t -> Edge.Set.t) : t -> Vertex.Set.t =
    fun graph ->
@@ -51,9 +34,11 @@ let (vertexes, live_vertexes) : (t -> Vertex.Set.t) * (t -> Vertex.Set.t) =
   in
   (impl edges, impl live_edges)
 
-let vertex (graph : t) (vertex_id : Uuid.Id.t) : Vertex.t option =
-  vertexes graph
-  |> Vertex.Set.find_first_opt (fun vertex -> vertex.id = vertex_id)
+let parent_vertexes (graph : t) (vertex : Vertex.t) : Vertex.Set.t =
+  Vertex.Set.empty
+  |> Edge.Set.fold
+       (fun e -> Vertex.Set.add e.value.source.vertex)
+       (parent_edges graph vertex)
 
 let orphaned (graph : t) : Vertex.Set.t =
   vertexes graph
@@ -75,3 +60,20 @@ let multiparented (graph : t) : Vertex.Set.t =
        (live_edges graph)
   |> Vertex.Map.filter (fun _ count -> count = 2)
   |> Vertex.Map.bindings |> List.map fst |> Vertex.Set.of_list
+
+let vertex (graph : t) (vertex_id : Uuid.Id.t) : Vertex.t option =
+  vertexes graph
+  |> Vertex.Set.find_first_opt (fun vertex -> vertex.id = vertex_id)
+
+(* S-Expression Conversions *)
+
+let sexp_of_t (graph : t) : Sexplib.Sexp.t =
+  Util.Sexp.of_map (Edge.Map.bindings graph) Edge.sexp_of_t Edge_state.sexp_of_t
+
+let t_of_sexp (sexp : Sexplib.Sexp.t) : t =
+  sexp
+  |> Sexplib.Std.list_of_sexp (function
+       | List [ key_sexp; value_sexp ] ->
+           (Edge.t_of_sexp key_sexp, Edge_state.t_of_sexp value_sexp)
+       | _ -> failwith __LOC__)
+  |> List.to_seq |> Edge.Map.of_seq

d25d43a0ec6aa8e5298610f6e62782305a3dbd4e
Eric Griffis <egriffis@umich.edu>
2020-12-11 03:13:39 -0500

    add documented Vertex.mli

---
+++ src/grvcore/graph/Vertex.mli
@@ -0,0 +1,17 @@
+(** Identifiable and comparable constructor instances. *)
+
+(** Vertices carry a unique identifier that allow structurally equivalent
+    instances to be distinguished and ordered. *)
+type t = Lang.Constructor.t Uuid.Wrap.t [@@deriving sexp]
+
+val mk : Lang.Constructor.t -> t
+(** [mk k] returns an instance of constructor [k]. *)
+
+val root : t
+(** The root vertex. *)
+
+(** {1 Collections} *)
+
+module Map : Map.S with type key = t
+
+module Set : Set.S with type elt = t

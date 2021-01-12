35ed2a2fa065264af03399d250bc9350ad5f0d58
Eric Griffis <egriffis@umich.edu>
2021-01-11 19:00:39 -0500

    odoc edit

--- src/grvcore/graph/Position_map.mli
+++ src/grvcore/graph/Position_map.mli
@@ -1,6 +1,6 @@
 (** Position maps *)
 
-(** A position map maps [Lang.Position.t]s to child representations. *)
+(** Maps [Lang.Position.t]s to child representations. *)
 include Map.S with type key = Lang.Position.t
 
 val get : ?default:'a list -> key -> 'a list t -> 'a list
